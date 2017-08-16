b7227545b5fd213cde9f7df83c1b148640de5d24
Dawnki <245357001@qq.com>
2017-08-16 13:45:29 +0800

    更改配置方式

--- config/server.php
+++ config/server.php
@@ -5,19 +5,23 @@
  * Date: 17-8-15
  * Time: 下午11:05
  */
-//服务器IP
-define('SERVER_IP','127.0.0.1');
-//服务器端口
-define('SERVER_PORT','9999');
-//工作进程数
-define('WORKER_NUM',4);
-//task数
-define('TASK_NUM',2);
-//是否纳入守护进程
-define('IS_DAEMON',false);
-//是否开启MQTT协议
-define('OPEN_MQTT',true);
-//1 轮循 2 固定 3 抢占 4 IP分配 5 UID分配
-define('DISPATCH_MOD',2);
-//最大请求数
-define('MAX_REQUEST',10000);
+
+return [
+    //服务器IP
+    'SERVER_IP' => '127.0.0.1',
+    //服务器端口
+    'SERVER_PORT' => '9999',
+    //工作进程数
+    'WORKER_NUM' => 4,
+    //task数
+    'TASK_NUM' => 2,
+    //是否纳入守护进程
+    'IS_DAEMON' => false,
+    //是否开启MQTT协议
+    'OPEN_MQTT' => true,
+    //1 轮循 2 固定 3 抢占 4 IP分配 5 UID分配
+    'DISPATCH_MOD' => 2,
+    //最大请求数
+    'MAX_REQUEST' => 10000
+
+];
