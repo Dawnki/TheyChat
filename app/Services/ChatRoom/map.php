e8826a593ef4c183d76f2dc278f89bf12a91b719
Dawnki <245357001@qq.com>
2017-08-18 15:00:49 +0800

    控制动作映射

---
+++ app/Services/ChatRoom/map.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dawnki
+ * Date: 17-8-18
+ * Time: 上午10:45
+ */
+
+return [
+    1 => 'Connect',
+    3 => 'Publish',
+    8 => 'Subscribe',
+    14 => 'Disconnect'
+];
