11f35b26a6290e5720501b61f71e3ac34d53c39b
Dawnki <245357001@qq.com>
2017-08-18 23:21:30 +0800

    关闭连接是删除fd与账号的关联

--- app/callback/onClose.php
+++ app/callback/onClose.php
@@ -11,8 +11,17 @@ namespace App\callback;
 
 class onClose
 {
+    protected $storage;
+
+    public function __construct()
+    {
+        $this->storage=app('storage');
+    }
+
     public function run($server, $fd, $reactor_id)
     {
+        $prefix=config('redis')['PREFIX']['connection'];
+        $this->storage->del($prefix.$fd);
         logger('fd:'.$fd.' has been disconnected!');
     }
 }
